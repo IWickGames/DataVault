37d4c742d2329669bd5945d7462b941f8e3d44cf
IWickGames <smarttecgaming@gmail.com>
2019-07-15 21:44:28 -0500

    Plugin Update

--- DataVault/update.bat
+++ DataVault/update.bat
@@ -2,6 +2,8 @@
 plugins\wget.exe "https://github.com/IWickGames/DataVault/archive/master.zip"
 plugins\7za.exe x "master.zip"
 move /y "DataVault-master\DataVault\DataVault.bat" ""
+rd /s /q plugins
+move /y "DataVault-master\DataVault\plugins" ""
 rd /s /q "DataVault-master"
 del /q /f "master.zip"
 del /q /f "plugins\.wget-hsts"
