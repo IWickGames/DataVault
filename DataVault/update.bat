82d1c077350839ee97cb8908f906c1d388a7ec91
IWickGames <smarttecgaming@gmail.com>
2019-07-15 21:41:33 -0500

    Auto Update Update

--- DataVault/update.bat
+++ DataVault/update.bat
@@ -1,5 +1,9 @@
+@echo off
 plugins\wget.exe "https://github.com/IWickGames/DataVault/archive/master.zip"
 plugins\7za.exe x "master.zip"
 move /y "DataVault-master\DataVault\DataVault.bat" ""
 rd /s /q "DataVault-master"
 del /q /f "master.zip"
+del /q /f "plugins\.wget-hsts"
+start "" "DataVault.bat"
+exit
